db846ad7930abd345bb5494b4ed60b980d93df9d
maxonhick <max2007varlamov@gmail.com>
2025-05-30 18:47:17 +0300

    change tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,80 +5,95 @@
 #include "Transaction.h"
 
 using ::testing::Return;
-using ::testing::Throw;
 using ::testing::_;
+using ::testing::AtLeast;
 
-class MyAccount : public Account {
+// Mock класс для Account
+class MockAccount : public Account {
 public:
-    MyAccount(int id, int balance) : Account(id, balance) {}
-    MOCK_CONST_METHOD0(GetBalance, int());
-    MOCK_METHOD1(ChangeBalance, void(int diff));
-    MOCK_METHOD0(Lock, void());
-    MOCK_METHOD0(Unlock, void());
+    MockAccount(int id, int balance) : Account(id, balance) {}
+    MOCK_METHOD(int, GetBalance, (), (const, override));
+    MOCK_METHOD(void, ChangeBalance, (int), (override));
+    MOCK_METHOD(void, Lock, (), (override));
+    MOCK_METHOD(void, Unlock, (), (override));
 };
 
-// Я подумал, что использование Gmock, нужно, но не везде, поэтому Transaction без него
-
-TEST(Account, Locker) {
-    MyAccount acc(0, 1111);
-	EXPECT_CALL(acc, Lock()).Times(2);
-	EXPECT_CALL(acc, Unlock()).Times(1);
-	acc.Lock();
-	acc.Lock();
-	acc.Unlock();
+// Тесты для Account
+TEST(Account, GetBalance_ReturnsCorrectValue) {
+    Account account(1, 1000);
+    EXPECT_EQ(account.GetBalance(), 1000);
 }
 
-TEST(Account, balance_positive) {
-    Account acc1(0, 1000);
-    EXPECT_EQ(acc1.GetBalance(), 1000);
-
-    acc1.Lock();
-    EXPECT_NO_THROW(acc1.ChangeBalance(100));
-
-    EXPECT_EQ(acc1.GetBalance(), 1100);
+TEST(Account, ChangeBalance_UpdatesBalance) {
+    Account account(1, 1000);
+    account.Lock();
+    account.ChangeBalance(500);
+    EXPECT_EQ(account.GetBalance(), 1500);
 }
 
-TEST(Accout, balance_negative) {
-    Account Vasya(1, 100);
-
-    EXPECT_THROW(Vasya.ChangeBalance(100), std::runtime_error);
-    
-    Vasya.Lock();
-    EXPECT_ANY_THROW(Vasya.Lock());
+TEST(Account, Lock_Unlock_WorksCorrectly) {
+    Account account(1, 1000);
+    account.Lock();
+    EXPECT_THROW(account.Lock(), std::runtime_error);
+    account.Unlock();
+    EXPECT_NO_THROW(account.Lock());
 }
 
-TEST(Transaction, construnct_and_positive) {
-    Transaction first;
-    EXPECT_EQ(first.fee(), 1);
+// Тесты для Transaction
+TEST(Transaction, Make_CallsAccountMethodsCorrectly) {
+    // Создаем mock-объекты для аккаунтов
+    MockAccount from(1, 1000);
+    MockAccount to(2, 500);
 
-    Account Petya(0, 6132);
-    Account Katya(1, 2133);
+    // Настраиваем ожидания для методов
+    EXPECT_CALL(from, Lock()).Times(1);
+    EXPECT_CALL(to, Lock()).Times(1);
+    EXPECT_CALL(from, Unlock()).Times(1);
+    EXPECT_CALL(to, Unlock()).Times(1);
+    EXPECT_CALL(from, ChangeBalance(-110)).Times(1);
+    EXPECT_CALL(to, ChangeBalance(100)).Times(1);
+    EXPECT_CALL(from, GetBalance()).Times(1).WillOnce(Return(1000));
+    EXPECT_CALL(to, GetBalance()).Times(1).WillOnce(Return(500));
 
-    first.set_fee(32);
-    EXPECT_EQ(first.fee(), 32);
-
-    EXPECT_TRUE(first.Make(Petya, Katya, 100));
-    EXPECT_EQ(Katya.GetBalance(), 2233);
-    EXPECT_EQ(Petya.GetBalance(), 6000);
-
+    // Выполняем транзакцию
+    Transaction transaction;
+    transaction.set_fee(10);
+    EXPECT_TRUE(transaction.Make(from, to, 100));
 }
 
-TEST(Transaction, negative) {
-    Transaction second;
-    second.set_fee(51);
-    Account Roma(0, 10);
-    Account Misha(1, 1000);
+TEST(Transaction, Make_ThrowsExceptionIfSumIsNegative) {
+    MockAccount from(1, 1000);
+    MockAccount to(2, 500);
 
-    EXPECT_THROW(second.Make(Misha, Misha, 0), std::logic_error);
+    Transaction transaction;
+    EXPECT_THROW(transaction.Make(from, to, -100), std::invalid_argument);
+}
 
-    EXPECT_THROW(second.Make(Misha, Roma, -100), std::invalid_argument);
+TEST(Transaction, Make_ThrowsExceptionIfSumIsTooSmall) {
+    MockAccount from(1, 1000);
+    MockAccount to(2, 500);
 
-    EXPECT_THROW(second.Make(Misha, Roma, 50), std::logic_error);
+    Transaction transaction;
+    EXPECT_THROW(transaction.Make(from, to, 50), std::logic_error);
+}
 
-    EXPECT_FALSE(second.Make(Misha, Roma, 100));
+TEST(Transaction, Make_ReturnsFalseIfFeeIsTooHigh) {
+    MockAccount from(1, 1000);
+    MockAccount to(2, 500);
 
-    second.set_fee(10);
+    Transaction transaction;
+    transaction.set_fee(1000);
+    EXPECT_FALSE(transaction.Make(from, to, 100));
+}
 
-    EXPECT_FALSE(second.Make(Roma, Misha, 100));
-    
+TEST(Transaction, Make_SavesToDatabaseCorrectly) {
+    MockAccount from(1, 1000);
+    MockAccount to(2, 500);
+
+    EXPECT_CALL(from, GetBalance()).Times(AtLeast(1)).WillRepeatedly(Return(1000));
+    EXPECT_CALL(to, GetBalance()).Times(AtLeast(1)).WillRepeatedly(Return(500));
+
+    Transaction transaction;
+    transaction.set_fee(10);
+    EXPECT_TRUE(transaction.Make(from, to, 100));
 }
